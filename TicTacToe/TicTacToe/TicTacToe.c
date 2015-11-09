e647f7090003e973bd8616f5ffd6b4ed8d7878ea
Saurav Sahu <sauravsahu.12@live.com>
2015-11-09 17:50:15 +0530

    Added Check Function. Still not complete.

--- TicTacToe/TicTacToe/TicTacToe.c
+++ TicTacToe/TicTacToe/TicTacToe.c
@@ -1,9 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 
+//Preprocessor Directives
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 #define ROWSIZE 3
 #define COLUMNSIZE 3
 
+//Data Structures
 enum Bool
 {
 	false,
@@ -23,6 +29,55 @@ struct Cell
 typedef enum Bool bool;
 typedef struct Cell cell;
 
+//Function Prototypes
+void Initialize(cell *[], int, int);
+bool IsValid(cell *[], int);
+void ReadUserName(char[], int);
+void Input(cell *[], int);
+void WriteLine(int);
+void Display(cell *[], char);
+int Toss();
+bool AreAllSame(cell [], int);
+
+//The main Function
+int main()
+{
+	//Variable Declaration
+	cell grid[3][3];
+	char user1[40], user2[40], temp[40];
+	int firstUser, count;
+
+	//Game Logic
+	do
+	{
+		count = 0;
+		ReadUserName(user1, 1);
+		ReadUserName(user2, 2);
+		firstUser = Toss();
+		switch (firstUser)
+		{
+		case 0: printf("%s won the toss.", user1);
+			break;
+		case 1: printf("%s won the toss.", user2);
+			strcpy(temp, user1);
+			strcpy(user1, user2);
+			strcpy(user2, temp);
+		}
+		printf("\nPLAYERS");
+		printf("\nPlayer 1: %s", user1);
+		printf("\nPlayer 2: %s", user2);
+
+		while (count < (ROWSIZE*COLUMNSIZE))
+		{
+			Display(grid, 'P');
+			Input(grid, count);
+			Display(grid, 'M');
+			++count;
+		}
+	} while (1);
+}
+
+//Function Definition
 void Initialize(cell grid[][COLUMNSIZE], int rowSize, int columnSize)
 {
 	int i, j;
@@ -43,12 +98,20 @@ bool IsValid(cell grid[][COLUMNSIZE], int position)
 	if ((grid[row][column]).isOccupied == true)return false;
 	return true;
 }
-void Input(cell grid[][COLUMNSIZE], char symbol)
+void ReadUserName(char name[], int num)
 {
+	printf("\nEnter player name %d: ", num);
+	gets(name);
+}
+void Input(cell grid[][COLUMNSIZE], int number)
+{
+	char symbol;
+	if (number % 2 == 0)symbol = 'X';
+	else symbol = 'O';
 	int position;
 	printf("\nEnter position to draw %c: ", symbol);
 	do
-	{ 
+	{
 		scanf("%d", &position);
 		if (IsValid(grid, position) == false)
 		{
@@ -56,9 +119,89 @@ void Input(cell grid[][COLUMNSIZE], char symbol)
 		}
 	} while (IsValid(grid, position) == false);
 }
-
-int main()
+void WriteLine(int rowSize)
 {
-	cell grid[3][3];
+	int i;
+	printf("\n");
+	for (i = 0; i < 2 * rowSize; i++)
+	{
+		if (i % 2 == 0) printf("_");
+		else printf(" ");
+	}
+}
+void Display(cell grid[][COLUMNSIZE], char mode)
+{
+	int i, j;
+	switch (mode)
+	{
+	case 'M':
+	{
+		WriteLine(ROWSIZE);
+		for (i = 0; i < ROWSIZE; i++)
+		{
+			printf("\n");
+			for (j = 0; j < COLUMNSIZE; j++)
+			{
+				if (grid[i][j].isOccupied == false)
+				{
+					printf("%d ", grid[i][j].position);
+				}
+				else
+				{
+					printf("%c ", grid[j][i].character);
+				}
+			}
+		}
+		WriteLine(ROWSIZE);
+	}
 
+	case 'P':
+	{
+		WriteLine(ROWSIZE);
+		for (i = 0; i < ROWSIZE; i++)
+		{
+			printf("\n");
+			for (j = 0; j < COLUMNSIZE; j++)
+			{
+				if (grid[i][j].isOccupied == false)
+				{
+					printf("%d ", grid[i][j].position);
+				}
+				else
+				{
+					printf("  ");
+				}
+			}
+		}
+		WriteLine(ROWSIZE);
+	}
+	}
+}
+int Toss()
+{
+	srand(time());
+	return (rand() % 2);
+}
+bool Check(cell grid[][COLUMNSIZE])
+{
+	int i, j;
+	for (i = 0; i < ROWSIZE; ++i)
+	{
+		for (j = 0; j < COLUMNSIZE; ++j)
+		{
+			//TODO
+		}
+	}
+}
+bool AreAllSame(cell array[], int size)
+{
+	int i;
+	for (i = 0; i < size - 1; ++i)
+	{
+		if (array[i].isOccupied == false
+			|| array[i+1].isOccupied == true
+			|| array[i+1].character!= array[i + 1].character)
+			return false;
+	}
+	return true;
 }
